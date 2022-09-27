11fef18a4ba7253028e72488e723252f089f6c27
jaehun jung <wognsddd777@naver.com>
2022-09-27 09:33:55 +0900

    modify loop for print all of var

--- 38_10971.py
+++ 38_10971.py
@@ -4,7 +4,7 @@ N = int(stdin.readline().split()[0])
 W = [list(map(int, stdin.readline().split())) for _ in range(N)]
 
 # 제대로 들어가는 지 확인
-# for i in range(N):
-#     for j in range(N):
-#         print(f'{W[i][j]:3}', end=' ')
-#     print()
+for i in range(len(W)):
+    for j in range(len(W[i])):
+        print(f'{W[i][j]:3}', end=' ')
+    print()
